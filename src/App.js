1c004dc552b17e0b243ef19f97efb4011c7d5695
Khue Doan <khuedoan98@gmail.com>
2020-12-27 23:37:50 +0700

    Add under construction notice

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>under construction</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/khuedoan"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
       </header>
     </div>
