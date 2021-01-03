07171aeccacfa581d5377f1da3937bab485612f3
Khue Doan <khuedoan98@gmail.com>
2021-01-03 17:06:39 +0700

    Squashed commit of the following:
    Revert "Add Ant Design"
    Revert "Add GitHub icon"
    Revert "Add basic layout"
    Revert "Fix test"

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import { Breadcrumb, Button, Layout, Menu, Space } from 'antd';
-import { GithubFilled, LinkedinFilled } from '@ant-design/icons';
 import './App.css';
-
-const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <Layout>
-      <Header>
-        <div className="logo" />
-        <Menu theme='dark' mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">Khue Doan</Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">
-          Coming soon
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        <Space>
-          <Button icon={<GithubFilled />} href="https://github.com/khuedoan" />
-          <Button icon={<LinkedinFilled />} href="https://linkedin.com/in/khuedoan" />
-        </Space>
-      </Footer>
-    </Layout>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>coming soon</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/khuedoan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </header>
+    </div>
   );
 }
 
