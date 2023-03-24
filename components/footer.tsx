fe3d5f9260172365be1adf6c042f147109ce5021
Khue Doan <mail@khuedoan.com>
2023-03-24 13:21:00 +0700

    style: apply CSS for footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; 2023 Khue Doan. All rights reserved.</p>
+      <div className="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
+        <p className="mt-5 text-center text-sm leading-5 text-gray-500">
+          &copy; {new Date().getFullYear()} Khue Doan. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
