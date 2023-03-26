c8e3d3e44b4fe47d020130f45cf8f10d0f1cafb7
Khue Doan <mail@khuedoan.com>
2023-03-26 21:02:32 +0700

    feat: add post preview and list

--- components/post-list.tsx
+++ components/post-list.tsx
@@ -5,15 +5,22 @@ export default function PostList() {
   const posts = getAllPosts();
 
   return (
-    <>
-      {posts.map((post) => (
-        <PostPreview
-          key={post.slug}
-          title={post.title}
-          slug={post.slug}
-          date={post.date.toString()}
-        />
-      ))}
-    </>
+    <div className="mx-auto max-w-3xl px-6 lg:px-8">
+      <div className="mt-16 space-y-10 lg:mt-10 lg:space-y-10">
+        {posts.map((post) => {
+          return (
+            <PostPreview
+              key={post.slug}
+              title={post.title}
+              slug={post.slug}
+              date={post.date.toLocaleString()}
+              summary={post.summary}
+              coverImage={post.coverImage}
+              tags={post.tags}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,22 +1,60 @@
 import Link from "next/link";
+import Image from "next/image";
+import Date from "./date";
 
 export default function PostPreview({
+  slug,
   title,
   date,
-  slug,
+  summary,
+  coverImage,
+  tags,
 }: {
+  slug: string;
   title: string;
   date: string;
-  slug: string;
+  summary: string;
+  coverImage: string;
+  tags: string[];
 }) {
   return (
-    <>
-      <div>
-        <Link href={`/posts/${slug}`}>{title}</Link>
+    <article
+      key={slug}
+      className="relative isolate flex flex-col gap-8 lg:flex-row"
+    >
+      <div className="relative aspect-[16/9] lg:w-64 lg:shrink-0">
+        <Image
+          className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+          src={`/images/${slug}/${coverImage}`}
+          alt=""
+          width="720"
+          height="1280"
+        />
+        <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
       </div>
-      <p>
-        {date}
-      </p>
-    </>
+      <div>
+        <div className="flex items-center gap-x-4 text-xs">
+          <Date dateString={date} />
+          {tags.map((tag: string) => (
+            <Link
+              key={tag}
+              href={`/tags/${tag}`}
+              className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
+            >
+              {tag}
+            </Link>
+          ))}
+        </div>
+        <div className="group relative max-w-xl">
+          <h2 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <Link href={`/posts/${slug}`}>
+              <span className="absolute inset-0" />
+              {title}
+            </Link>
+          </h2>
+          <p className="mt-5 text-sm leading-6 text-gray-600">{summary}</p>
+        </div>
+      </div>
+    </article>
   );
 }
