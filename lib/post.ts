c4b06165549a1ae9577ad0793e087a4d4e71bd7e
Khue Doan <mail@khuedoan.com>
2023-03-26 13:59:36 +0700

    feat: hide draft posts

--- lib/post.ts
+++ lib/post.ts
@@ -41,6 +41,7 @@ export function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
+    .filter((post) => !post.draft) // TODO auto show draft posts in dev mode
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
 
