8700c5c7c9185fd7b29f509f28743f5c4f0a96e3
Khue Doan <mail@khuedoan.com>
2023-03-27 10:52:30 +0700

    style: add padding to post header

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -8,13 +8,13 @@ export default function PostHeader({
   date: string;
 }) {
   return (
-    <>
+    <div className="py-6">
       <p className="text-base font-semibold leading-7 text-indigo-600">
         <Date dateString={date} />
       </p>
       <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
         {title}
       </h1>
-    </>
+    </div>
   );
 }
