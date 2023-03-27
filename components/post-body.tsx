b745b59d2b7df37ea5e65fbbb3a07ef4fed9ad84
Khue Doan <mail@khuedoan.com>
2023-03-27 16:37:16 +0700

    feat: add GFM

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,8 +1,10 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 export default function PostBody({ content }: { content: string }) {
   return (
     <article className="prose max-w-none">
-      <ReactMarkdown children={content} />
+      <ReactMarkdown children={content} remarkPlugins={[remarkGfm]} />
     </article>
   );
 }
