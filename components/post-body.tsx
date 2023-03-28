5fd1e2e6aa4dabe7e506355228c8dc77a15670e8
Khue Doan <mail@khuedoan.com>
2023-03-28 19:11:21 +0700

    fix: disable react/jsx-no-comment-textnodes for ReactMarkdown

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -4,6 +4,7 @@ import remarkGfm from "remark-gfm";
 export default function PostBody({ content }: { content: string }) {
   return (
     <article className="prose max-w-none">
+      {/* eslint-disable-next-line react/no-children-prop */}
       <ReactMarkdown children={content} remarkPlugins={[remarkGfm]} />
     </article>
   );
