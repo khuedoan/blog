48c8d78e925584f7eb06ff79ddc33e249c8059f8
Khue Doan <mail@khuedoan.com>
2024-09-30 21:48:58 +0700

    chore: upgrade packages

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
-  description = "Khue Doan's Blog";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
