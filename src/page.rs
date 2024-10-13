0bb67954ffae237c37854fd806facb6a0189c318
Khue Doan <mail@khuedoan.com>
2024-10-13 13:52:11 +0700

    fix: adjust based on Lighthouse recommendations
    - Proper head and body
    - Set language
    - Add description
    - Defer HTMX script to make it non-blocking

--- src/page.rs
+++ src/page.rs
@@ -1,34 +1,41 @@
 use maud::{html, Markup, DOCTYPE};
 
-fn header(title: &str) -> Markup {
+fn head(title: &str) -> Markup {
     html! {
-        (DOCTYPE)
-        meta charset="utf-8";
-        meta name="viewport" content="width=device-width, initial-scale=1";
-        meta name="color-scheme" content="light dark";
-        link rel="stylesheet" href="/pico.min.css";
-        script src="/htmx.min.js" {}
-        title { (title) }
+        head {
+            meta charset="utf-8";
+            meta name="description" content="Khue's personal website";
+            meta name="viewport" content="width=device-width, initial-scale=1";
+            meta name="color-scheme" content="light dark";
+            link rel="stylesheet" href="/pico.min.css";
+            script src="/htmx.min.js" defer {}
+            title { (title) }
+        }
     }
 }
 
 pub fn base(title: &str, body: Markup) -> Markup {
     html! {
-        (header(title))
-        header class="container" {
-            nav {
-                ul {
-                    li { strong { a href="/" { "Khue Doan" } } }
+        (DOCTYPE)
+        html lang="en" {
+            (head(title))
+            body {
+                header class="container" {
+                    nav {
+                        ul {
+                            li { strong { a href="/" { "Khue Doan" } } }
+                        }
+                        ul {
+                            li { a href="/contact" { "Contact" } }
+                            li { a href="/about" { "About" } }
+                        }
+                    }
                 }
-                ul {
-                    li { a href="/contact" { "Contact" } }
-                    li { a href="/about" { "About" } }
+                main class="container" {
+                    h1 { (title) }
+                    (body)
                 }
             }
-        }
-        main class="container" {
-            h1 { (title) }
-            (body)
         }
     }
 }
